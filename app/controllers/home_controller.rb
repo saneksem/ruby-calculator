de3d0bcf56b33849198a3716654636354b60b1d4
Александр Семочко <saneksem@gmail.com>
2016-05-01 16:25:51 +0300

    New result output format

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,20 +3,18 @@ require File.expand_path('../../lib/calculator', File.dirname(__FILE__))
 class HomeController < ApplicationController
 
   def index
-    @result = 'Calculation result: '
     case params[:operation]
       when 'sum', 'subtr', 'div'
-        @result = Calculator.send(params[:operation], *[params[:a], params[:b]])
-      when 'sqr', 'sin', 'cos'
-        @result = Calculator.send(params[:operation], *[params[:a]])
+        @result = Calculator.fix_int(params[:a]).to_s + ' ' +
+            Calculator.get_operator(params[:operation]) + ' ' +
+            Calculator.fix_int(params[:b]).to_s + ' = ' +
+            Calculator.send(params[:operation], *[params[:a], params[:b]]).to_s
+      when 'sqrt', 'sin', 'cos'
+        @result = Calculator.get_operator(params[:operation]) + '(' + Calculator.fix_int(params[:a]).to_s + ') = ' +
+            Calculator.send(params[:operation], *[params[:a]]).to_s
       else
         @result = nil
     end
   end
 
-  def new
-    @result = (params[:a]=="" or params[:b]=="") ? nil : params[:a].to_f + params[:b].to_f
-    @result = Calculator.send(params[:operation], *[params[:a], params[:b]])
-    render :index
-  end
 end
--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,20 @@
-
 module Calculator
+
   def self.fix_int(a)
     (a % 1)== 0 ? a.to_i : a
+  end
+
+  def self.get_operator(a)
+    case a
+      when 'sum'
+        '+'
+      when 'subtr'
+        '-'
+      when 'div'
+        '/'
+      else
+        a
+    end
   end
 
   def self.sum(a, b)
@@ -16,7 +29,7 @@ module Calculator
     fix_int(a.to_f / b.to_f)
   end
 
-  def self.sqr(a)
+  def self.sqrt(a)
     fix_int(Math.sqrt(a.to_f))
   end
 
@@ -27,4 +40,5 @@ module Calculator
   def self.cos(a)
     fix_int(Math.cos(a.to_f))
   end
+  
 end

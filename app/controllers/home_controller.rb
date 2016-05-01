0ef204a70d66402c0ec2c8d8ec141bfcd439ad35
Александр Семочко <saneksem@gmail.com>
2016-05-01 20:02:52 +0300

    Added fields validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,24 @@ require File.expand_path('../../lib/calculator', File.dirname(__FILE__))
 class HomeController < ApplicationController
 
   def index
+    @result = ''
     case params[:operation]
       when 'sum', 'subtr', 'div'
-        @result = Calculator.fix_int(params[:a]).to_s + ' ' +
-            Calculator.get_operator(params[:operation]) + ' ' +
-            Calculator.fix_int(params[:b]).to_s + ' = ' +
-            Calculator.send(params[:operation], *[params[:a], params[:b]]).to_s
+        if params[:a].to_s=='' or params[:b].to_s==''
+          @result = 'Please, enter both operands!'
+        else
+          @result = Calculator.fix_int(params[:a]).to_s + ' ' +
+              Calculator.get_operator(params[:operation]) + ' ' +
+              Calculator.fix_int(params[:b]).to_s + ' = ' +
+              Calculator.send(params[:operation], *[params[:a], params[:b]]).to_s
+        end
       when 'sqrt', 'sin', 'cos'
-        @result = Calculator.get_operator(params[:operation]) + '(' + Calculator.fix_int(params[:a]).to_s + ') = ' +
-            Calculator.send(params[:operation], *[params[:a]]).to_s
+        if params[:a].to_s==''
+          @result += 'Please, enter first operand!'
+        else
+          @result = Calculator.get_operator(params[:operation]) + '(' + Calculator.fix_int(params[:a]).to_s + ') = ' +
+              Calculator.send(params[:operation], *[params[:a]]).to_s
+        end
       else
         @result = nil
     end
